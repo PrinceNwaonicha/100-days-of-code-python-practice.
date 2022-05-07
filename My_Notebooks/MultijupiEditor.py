ead479c76105849a690e6d4b84356c138c9c361d
Prince Nwaonicha <lolgfman@gmail.com>
2022-05-06 19:25:14 -0500

    Cleaned up Editor code

--- My_Notebooks/MultijupiEditor.py
+++ My_Notebooks/MultijupiEditor.py
@@ -2,6 +2,8 @@ import json
 import os
 from pprint import pprint
 import codecs
+
+# Start on day 2 cause I made this on my second day.
 day = 2
 
 for filename in os.scandir("../notebooks/"):
@@ -14,18 +16,25 @@ for filename in os.scandir("../notebooks/"):
             tupl, v1, v2, v4 = data.items()
             # cells is a list of dictionaries
             cells = tupl[1]
-            newcell = []
             for item in cells:
                 if item['cell_type'] == 'code':
                     del cells[x]
                     x += 1
                 else:
-                    for words in range(len(item['source'])):
-                        if 'Solutions' in item['source'][words]:
-                            del cells[x]['source'][-1]
+                    try:
+                        # trys to remove solutions if its not there then continue
+                        if item['cell_type'] == 'markdown':
+                            cells[x]['source'].remove('**Solutions:**')
+                        else:
+                            x += 1
+                            continue
+                    except ValueError:
+                        x += 1
+                        continue
                     x += 1
-            # dump data into new file
 
+            # dump data into new file
             out_file = codecs.open(f"./My_Day{day}.ipynb", "w")
             json.dump(data, out_file, indent="")
+
     day += 1
